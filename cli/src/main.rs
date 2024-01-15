d0039dcbdce38dd65d19209ae1c1248d208267ba
Kylee Tilley <kyleetilley@gmail.com>
2024-01-14 23:16:29 -0600

    Fix name of cli when calling --version

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -6,7 +6,7 @@ use lexer::Lexer;
 
 /// Run a request file
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
+#[command(name="reqlang", author, version, about, long_about = None)]
 struct Args {
     /// Path to request file
     path: String,
@@ -24,7 +24,7 @@ fn main() {
     let document = match parser.parse(lexer) {
         Ok(program) => program,
         Err(err) => {
-            eprintln!("Errors parsing file: {:?}", err);
+            eprintln!("There were errors parsing request file:\n\n{:#?}", err);
             exit(1);
         }
     };
