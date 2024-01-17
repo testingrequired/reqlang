fc6583502a805a39e74f5d50294b7732560ff40a
Kylee Tilley <kyleetilley@gmail.com>
2024-01-16 20:27:57 -0600

    Implement splitter in new parser

--- types/src/lib.rs
+++ types/src/lib.rs
@@ -1,24 +1,63 @@
-#[derive(Clone, Debug, PartialEq)]
+use std::collections::HashMap;
+
+#[derive(Clone, Debug, PartialEq, Default)]
+pub struct Document {
+    pub request: Request,
+}
+
+#[derive(Clone, Debug, PartialEq, Default)]
 pub struct Request {
-    pub verb: Verb,
+    pub verb: String,
     pub target: String,
-    pub http_version: HttpVersion,
+    pub http_version: String,
+    pub headers: Vec<(String, String)>,
+    pub body: Option<String>,
 }
 
-#[derive(Clone, Debug, PartialEq)]
-pub enum Verb {
-    GET,
-    POST,
-    PUT,
-    DELETE,
-    HEAD,
-    OPTIONS,
-    PATCH,
-    CONNECT,
-    TRACE,
+/// A request file with an environement applied and values (vars, prompts, secrets)
+/// resolved
+#[derive(Clone, Debug, PartialEq, Default)]
+pub struct RequestFile {
+    pub config: RequestFileConfig,
+    pub request: String,
+    pub response: Option<String>,
 }
 
-#[derive(Clone, Debug, PartialEq)]
-pub enum HttpVersion {
-    OneOne,
+/// A request file config with an environement applied and values (vars, prompts, secrets)
+/// resolved
+#[derive(Clone, Debug, PartialEq, Default)]
+pub struct RequestFileConfig {
+    pub env: String,
+    pub vars: HashMap<String, String>,
+    pub prompts: HashMap<String, String>,
+    pub secrets: HashMap<String, String>,
+}
+
+/// A template file without an environement set or templating applied
+#[derive(Clone, Debug, PartialEq, Default)]
+pub struct TemplateFileUntemplated {
+    pub config: TemplateFileUntemplatedConfig,
+}
+
+/// A template file config without an environement set or templating applied
+#[derive(Clone, Debug, PartialEq, Default)]
+pub struct TemplateFileUntemplatedConfig {
+    pub template: Option<String>,
+}
+
+/// A template file with an environement applied and values (vars, prompts, secrets)
+/// resolved
+#[derive(Clone, Debug, PartialEq, Default)]
+pub struct TemplateFile {
+    pub config: TemplateFileConfig,
+}
+
+/// A template file config with an environement applied and values (vars, prompts, secrets)
+/// resolved
+#[derive(Clone, Debug, PartialEq, Default)]
+pub struct TemplateFileConfig {
+    pub env: String,
+    pub vars: HashMap<String, String>,
+    pub prompts: HashMap<String, String>,
+    pub secrets: HashMap<String, String>,
 }
