5c55bbae2644b7324b1bb45702abe75f6b82c823
Kylee Tilley <kyleetilley@gmail.com>
2024-01-24 23:45:04 -0600

    Implement diagnostics in language server

--- reqlang-lsp/src/main.rs
+++ reqlang-lsp/src/main.rs
@@ -1,12 +1,13 @@
+use std::ops::Deref;
+
 use anyhow::{Context, Result};
-use tower_lsp::{
-    jsonrpc,
-    lsp_types::{
-        DidChangeTextDocumentParams, DidOpenTextDocumentParams, InitializeParams, InitializeResult,
-        MessageType, ServerCapabilities, ServerInfo, TextDocumentSyncKind,
-    },
-    Client, LanguageServer, LspService, Server,
+use diagnostics::{Diagnoser, Diagnosis, DiagnosisPosition, DiagnosisRange, DiagnosisSeverity};
+use tower_lsp::lsp_types::{
+    Diagnostic, DiagnosticSeverity, DidChangeTextDocumentParams, DidOpenTextDocumentParams,
+    InitializeParams, InitializeResult, MessageType, Position, Range, ServerCapabilities,
+    ServerInfo, TextDocumentSyncKind,
 };
+use tower_lsp::{jsonrpc, Client, LanguageServer, LspService, Server};
 
 struct Backend {
     client: Client,
@@ -47,15 +48,130 @@ impl LanguageServer for Backend {
     }
 
     async fn did_open(&self, params: DidOpenTextDocumentParams) {
-        let _source = &params.text_document.text;
-        let _uri = params.text_document.uri;
-        let _version = Some(params.text_document.version);
+        let source = &params.text_document.text;
+        let uri = params.text_document.uri;
+        let version = Some(params.text_document.version);
+        let diagnostics = Diagnoser::get_diagnostics(source);
+        self.client
+            .publish_diagnostics(
+                uri,
+                diagnostics
+                    .iter()
+                    .map(|x| (LspDiagnosis((*x).clone())).into())
+                    .collect(),
+                version,
+            )
+            .await;
     }
 
     async fn did_change(&self, params: DidChangeTextDocumentParams) {
-        let _source = &params.content_changes.first().unwrap().text;
-        let _uri = params.text_document.uri;
-        let _version = Some(params.text_document.version);
+        let source = &params.content_changes.first().unwrap().text;
+        let uri = params.text_document.uri;
+        let version = Some(params.text_document.version);
+        let diagnostics = Diagnoser::get_diagnostics(source);
+        self.client
+            .publish_diagnostics(
+                uri,
+                diagnostics
+                    .iter()
+                    .map(|x| LspDiagnosis((*x).clone()).into())
+                    .collect(),
+                version,
+            )
+            .await;
+    }
+}
+
+struct LspDiagnosis(Diagnosis);
+impl Deref for LspDiagnosis {
+    type Target = Diagnosis;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl From<LspDiagnosis> for Diagnostic {
+    fn from(value: LspDiagnosis) -> Self {
+        let range: LspDiagnosisRange = value.range.into();
+
+        Diagnostic {
+            range: range.into(),
+            severity: value.severity.map(|x| LspDiagnosisSeverity(x).into()),
+            message: value.message.clone(),
+            ..Default::default()
+        }
+    }
+}
+
+struct LspDiagnosisPosition(DiagnosisPosition);
+impl Deref for LspDiagnosisPosition {
+    type Target = DiagnosisPosition;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl From<LspDiagnosisPosition> for Position {
+    fn from(value: LspDiagnosisPosition) -> Self {
+        Position {
+            line: value.line,
+            character: value.character,
+        }
+    }
+}
+impl From<DiagnosisPosition> for LspDiagnosisPosition {
+    fn from(value: DiagnosisPosition) -> Self {
+        LspDiagnosisPosition(value)
+    }
+}
+
+struct LspDiagnosisRange(DiagnosisRange);
+impl Deref for LspDiagnosisRange {
+    type Target = DiagnosisRange;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl From<LspDiagnosisRange> for Range {
+    fn from(value: LspDiagnosisRange) -> Self {
+        let start: LspDiagnosisPosition = value.start.into();
+        let end: LspDiagnosisPosition = value.end.into();
+
+        Range {
+            start: start.into(),
+            end: end.into(),
+        }
+    }
+}
+impl From<DiagnosisRange> for LspDiagnosisRange {
+    fn from(value: DiagnosisRange) -> Self {
+        LspDiagnosisRange(value)
+    }
+}
+
+struct LspDiagnosisSeverity(DiagnosisSeverity);
+impl Deref for LspDiagnosisSeverity {
+    type Target = DiagnosisSeverity;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl From<LspDiagnosisSeverity> for DiagnosticSeverity {
+    fn from(value: LspDiagnosisSeverity) -> Self {
+        match value.0 {
+            DiagnosisSeverity::ERROR => DiagnosticSeverity::ERROR,
+            DiagnosisSeverity::HINT => DiagnosticSeverity::HINT,
+            DiagnosisSeverity::INFORMATION => DiagnosticSeverity::INFORMATION,
+            DiagnosisSeverity::WARNING => DiagnosticSeverity::WARNING,
+            _ => panic!("Invalid diagnosis severity {:?}", value.0),
+        }
+    }
+}
+impl From<DiagnosisSeverity> for LspDiagnosisSeverity {
+    fn from(value: DiagnosisSeverity) -> Self {
+        LspDiagnosisSeverity(value)
     }
 }
 
