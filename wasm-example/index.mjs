7de4b9719babe87ace0428eca4506d25392f2f6b
Kylee Tilley <kyleetilley@gmail.com>
2024-01-27 03:02:52 -0600

    Fix

--- wasm-example/index.mjs
+++ wasm-example/index.mjs
@@ -1,7 +1,14 @@
 import * as reqlang from "@testingrequired/reqlang-wasm";
 
 const reqfile = `
+---
+GET / HTTP/1.1
+host: {{:base_url}}
 
+---
+---
+vars = ["base_url"]
+---
 `;
 
 console.log(JSON.stringify(reqlang.parse(reqfile), null, 2));
