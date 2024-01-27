68c336f452521bed13cbbede7cab670d60816e01
Kylee Tilley <kyleetilley@gmail.com>
2024-01-27 05:09:08 -0600

    Add doc comments to wasm functions

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -2,6 +2,11 @@ use std::collections::HashMap;
 
 use wasm_bindgen::prelude::*;
 
+/// Parse a string in to a request file with unresoling template values
+///
+/// # Arguments
+///
+/// * `source` - String to parse
 #[wasm_bindgen]
 #[allow(non_snake_case)]
 pub fn parse(source: &str) -> JsValue {
@@ -15,6 +20,14 @@ pub fn parse(source: &str) -> JsValue {
     }
 }
 
+/// Parse a string in to a request file and resolve template values
+///
+/// # Arguments
+///
+/// * `source` - String to parse
+/// * `env` - Environment to use when resolving variables
+/// * `prompts` - Prompt values
+/// * `secrets` - Secret values
 #[wasm_bindgen]
 #[allow(non_snake_case)]
 pub fn resolve(source: &str, env: &str, prompts: JsValue, secrets: JsValue) -> JsValue {
