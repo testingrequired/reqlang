f288ba350f1825b43464d3d835686ff7453775ff
Kylee Tilley <kyleetilley@gmail.com>
2024-02-01 12:22:22 -0600

    Add per reqfile env selection in vsc ext

--- vsc/extension.ts
+++ vsc/extension.ts
@@ -17,9 +17,15 @@ import {
   LanguageClientOptions,
   ServerOptions,
 } from "vscode-languageclient/node";
+import { Disposable } from "vscode";
 
 let lc: LanguageClient;
 let status: StatusBarItem;
+let activeTextEditorHandler: Disposable;
+
+type ReqfileState = {
+  env: string;
+};
 
 export function activate(context: ExtensionContext) {
   const serverOptions: ServerOptions = {
@@ -77,28 +83,55 @@ export function activate(context: ExtensionContext) {
   };
 
   const setResolverEnv = async () => {
-    const env = await window.showInputBox({
-      title: "Set the env for request file resolver to use",
-      prompt: "Leave empty to clear the env",
-    });
+    const env =
+      (await window.showInputBox({
+        title: "Set the env for request file resolver to use",
+        prompt: "Leave empty to clear the env",
+      })) ?? "";
 
     if (env.length === 0) {
       return clearResolverEnv();
     }
 
-    context.workspaceState.update("env", env);
+    const state: ReqfileState = {
+      env,
+    };
+
+    if (!window.activeTextEditor) {
+      return;
+    }
+
+    context.workspaceState.update(
+      window.activeTextEditor.document.fileName,
+      state
+    );
 
     updateStatusText();
   };
 
   const clearResolverEnv = async () => {
-    context.workspaceState.update("env", undefined);
+    if (!window.activeTextEditor) {
+      return;
+    }
+
+    context.workspaceState.update(
+      window.activeTextEditor.document.fileName,
+      undefined
+    );
 
     updateStatusText();
   };
 
   function updateStatusText() {
-    const env = context.workspaceState.get("env");
+    if (!window.activeTextEditor) {
+      return;
+    }
+
+    const state: ReqfileState | undefined = context.workspaceState.get(
+      window.activeTextEditor.document.fileName
+    );
+
+    const env = state?.env;
     const text = typeof env === "undefined" ? "REQLANG" : `REQLANG(${env})`;
 
     status.text = text;
@@ -140,9 +173,48 @@ export function activate(context: ExtensionContext) {
   );
 
   tasks.registerTaskProvider("reqlang", new ReqlangTaskProvider());
+
+  function handleTextEditorChange() {
+    if (!window.activeTextEditor) {
+      return;
+    }
+
+    let filename = window.activeTextEditor.document.fileName;
+
+    if (!filename.endsWith(".reqlang")) {
+      return;
+    }
+
+    let state: ReqfileState;
+
+    let existingState = context.workspaceState.get<ReqfileState>(
+      window.activeTextEditor.document.fileName
+    );
+
+    if (typeof existingState === "undefined") {
+      context.workspaceState.update(
+        window.activeTextEditor.document.fileName,
+        {}
+      );
+
+      state = context.workspaceState.get<ReqfileState>(
+        window.activeTextEditor.document.fileName
+      ) as ReqfileState;
+    } else {
+      state = existingState;
+    }
+
+    updateStatusText();
+  }
+
+  activeTextEditorHandler = window.onDidChangeActiveTextEditor(
+    handleTextEditorChange
+  );
 }
 
 export function deactivate() {
+  activeTextEditorHandler?.dispose();
+
   if (!lc) {
     return undefined;
   }
