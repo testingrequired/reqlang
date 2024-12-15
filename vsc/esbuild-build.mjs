1379ea70db0a321d7d94788351da833d2b75c9df
Kylee Tilley <kyleetilley@gmail.com>
2024-12-15 02:11:44 -0600

    Improve vsc ext build

--- vsc/esbuild-build.mjs
+++ vsc/esbuild-build.mjs
@@ -1,16 +1,57 @@
-import { build } from "esbuild";
+import { context } from "esbuild";
+
+const production = process.argv.includes("--production");
+const watch = process.argv.includes("--watch");
+
+async function main() {
+  const ctx = await context({
+    entryPoints: ["extension.ts"],
+    bundle: true,
+    format: "cjs",
+    minify: production,
+    sourcemap: !production,
+    sourcesContent: false,
+    platform: "node",
+    outfile: "out/extension.js",
+    external: ["vscode"],
+    logLevel: "silent",
+    treeShaking: true,
+    plugins: [
+      /* add to the end of plugins array */
+      esbuildProblemMatcherPlugin,
+    ],
+  });
+  if (watch) {
+    await ctx.watch();
+  } else {
+    await ctx.rebuild();
+    await ctx.dispose();
+  }
+}
 
 /**
- * @type {import("esbuild").BuildOptions}
+ * @type {import('esbuild').Plugin}
  */
-export const buildConfig = {
-  entryPoints: ["extension.ts"],
-  bundle: true,
-  outfile: "out/extension.js",
-  external: ["vscode"],
-  format: "cjs",
-  platform: "node",
-  sourcemap: true,
+const esbuildProblemMatcherPlugin = {
+  name: "esbuild-problem-matcher",
+
+  setup(build) {
+    build.onStart(() => {
+      console.log("[watch] build started");
+    });
+    build.onEnd((result) => {
+      result.errors.forEach(({ text, location }) => {
+        console.error(`✘ [ERROR] ${text}`);
+        console.error(
+          `    ${location.file}:${location.line}:${location.column}:`
+        );
+      });
+      console.log("[watch] build finished");
+    });
+  },
 };
 
-await build(buildConfig);
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
--- vsc/esbuild-watch.mjs
+++
@@ -1,6 +0,0 @@
-import { context } from "esbuild";
-import { buildConfig } from "./esbuild-build.mjs";
-
-const ctx = await context(buildConfig);
-
-await ctx.watch();
