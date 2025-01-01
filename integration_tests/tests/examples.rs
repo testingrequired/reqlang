050b5a83673557d64717d58515db652d7ed2eb14
Kylee Tilley <kyleetilley@gmail.com>
2025-01-01 00:20:35 -0600

    Refactor

--- integration_tests/tests/examples.rs
+++ integration_tests/tests/examples.rs
@@ -28,16 +28,12 @@ async fn integration_status_code_reqfile() {
     let path = PathBuf::from("../examples/valid/status_code.reqlang");
     let source = fs::read_to_string(path).expect("unable to read test file");
 
-    let mut prompts = HashMap::new();
-
-    prompts.insert("status_code".to_string(), "201".to_string());
-
+    let env = "default";
+    let prompts = HashMap::from([("status_code".to_string(), "201".to_string())]);
     let secrets = HashMap::new();
-    let mut provider_values = HashMap::new();
+    let provider_values = HashMap::from([("env".to_string(), "default".to_string())]);
 
-    provider_values.insert("env".to_string(), "default".to_string());
-
-    let reqfile = template(&source, "default", &prompts, &secrets, provider_values)
+    let reqfile = template(&source, env, &prompts, &secrets, provider_values)
         .expect("Unable to template request file");
 
     let fetcher: HttpRequestFetcher = reqfile.request.into();
