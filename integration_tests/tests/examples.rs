ae068ea7bced9e633c0e2b8e251f0be0ceebf4ca
Kylee Tilley <kyleetilley@gmail.com>
2025-01-02 01:13:30 -0600

    Refactor

--- integration_tests/tests/examples.rs
+++ integration_tests/tests/examples.rs
@@ -11,14 +11,14 @@ use reqlang::{
 
 #[rstest::rstest]
 fn integration_valid(#[files("../examples/valid/*.reqlang")] path: PathBuf) {
-    let source = fs::read_to_string(path).expect("Should get text from file");
+    let source = fs::read_to_string(path).expect("text should have been read from file");
 
     assert!(reqlang::parse(&source).is_ok());
 }
 
 #[rstest::rstest]
 fn integration_invalid(#[files("../examples/invalid/*.reqlang")] path: PathBuf) {
-    let source = fs::read_to_string(path).expect("Should get text from file");
+    let source = fs::read_to_string(path).expect("text should have been read from file");
 
     assert!(reqlang::parse(&source).is_err());
 }
@@ -26,7 +26,7 @@ fn integration_invalid(#[files("../examples/invalid/*.reqlang")] path: PathBuf) 
 #[tokio::test]
 async fn integration_status_code_reqfile() {
     let path = PathBuf::from("../examples/valid/status_code.reqlang");
-    let source = fs::read_to_string(path).expect("unable to read test file");
+    let source = fs::read_to_string(path).expect("text should have been read from file");
 
     let env = "default";
     let prompts = HashMap::from([("status_code".to_string(), "201".to_string())]);
@@ -34,11 +34,11 @@ async fn integration_status_code_reqfile() {
     let provider_values = HashMap::from([("env".to_string(), "default".to_string())]);
 
     let reqfile = template(&source, env, &prompts, &secrets, provider_values)
-        .expect("Request files should have templated");
+        .expect("request file should have been templated");
 
     let fetcher: HttpRequestFetcher = reqfile.request.into();
 
-    let response: HttpResponse = fetcher.fetch().await.expect("Request should have executed");
+    let response: HttpResponse = fetcher.fetch().await.expect("request should have executed");
 
     assert_eq!(HttpVersion::one_point_one(), response.http_version);
     assert_eq!(HttpStatusCode::new(201), response.status_code);
